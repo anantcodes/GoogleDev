570a8c0ceaacb08a9be91622c5d5adb0c5e806a9
Anant Kanchan <anant4265@gmail.com>
2020-09-18 08:27:38 +0530

    initial commit message

--- app/src/main/java/com/anant/googledev/MainActivity.kt
+++ app/src/main/java/com/anant/googledev/MainActivity.kt
@@ -2,10 +2,19 @@ package com.anant.googledev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var btnSearch:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSearch=findViewById(R.id.btnSearch)
+        
     }
 }
