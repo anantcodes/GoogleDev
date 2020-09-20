ef8f395e86175abf0675e8d2fb15352765317463
Anant Kanchan <anant4265@gmail.com>
2020-09-20 19:44:57 +0530

    add

--- app/src/main/java/com/anant/googledev/DisplayActivity.kt
+++ app/src/main/java/com/anant/googledev/DisplayActivity.kt
@@ -1,11 +1,24 @@
 package com.anant.googledev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DisplayActivity : AppCompatActivity() {
+
+    lateinit var btnBack: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+
+        btnBack=findViewById(R.id.btnBack)
+        
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
